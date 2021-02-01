9a045cb16a6a98354d5bb488b197042262ff80e1
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-31 19:51:00 -0800

    Upper method; does not change original string, creates a new string

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-# name = input("What is your name? ")
-# print("Hello, " + name)
+course = 'Python for Beginners'
 
-# birthYear = input("Enter your birth year: ")
-# age = 2020 - int(birthYear)
-# print(age)
-
-int() #Converts to integer
-float() #Convert to floating integer (10.1)
-bool() #Convert to boolean
-str() #Convert to string
-
-num1 = float(input("Pick a number: "))
-num2 = float(input("Pick another number: "))
-sum = num1 + num2
-print("sum is: " + str(sum))
+print(course.upper())
