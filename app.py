a72ac413036e41e306ff9205bcda71f4ea71b8ce
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-31 17:16:54 -0800

    Conversion functions

--- app.py
+++ app.py
@@ -4,3 +4,8 @@
 birthYear = input("Enter your birth year: ")
 age = 2020 - int(birthYear)
 print(age)
+
+int() #Converts to integer
+float() #Convert to floating integer (10.1)
+bool() #Convert to boolean
+str() #Convert to string
