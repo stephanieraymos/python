3df71bd95a6e386f6e50f60190f2912f58d149b2
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-31 17:24:58 -0800

    Getting sum of two nums from input

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # name = input("What is your name? ")
 # print("Hello, " + name)
 
-birthYear = input("Enter your birth year: ")
-age = 2020 - int(birthYear)
-print(age)
+# birthYear = input("Enter your birth year: ")
+# age = 2020 - int(birthYear)
+# print(age)
 
 int() #Converts to integer
 float() #Convert to floating integer (10.1)
 bool() #Convert to boolean
 str() #Convert to string
+
+num1 = input("Pick a number: ")
+num2 = input("Pick another number: ")
+sum = int(num1) + int(num2)
+print(sum)
