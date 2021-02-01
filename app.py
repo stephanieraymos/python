c57c6be45eaf7e29946a14d68d966fa2bff94a9e
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-31 17:12:52 -0800

    int function to switch data type to integer

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
-name = input("What is your name? ")
-print("Hello, " + name)
+# name = input("What is your name? ")
+# print("Hello, " + name)
+
+birthYear = input("Enter your birth year: ")
+age = 2020 - int(birthYear)
+print(age)
