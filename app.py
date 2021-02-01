a83a23562235ac87238756d6a16e7e4bc85f9f2d
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-01-31 20:00:00 -0800

    Assignment operators

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-course = 'Python for Beginners'
+#print(10 // 3)
+# / Returns exact division, // returns a whole number rounded down
+# print(10 % 3) Modulous operator
+# Returns remainder of the division
+# print(10 ** 3) Exponent operator; 10 to the power of 3
 
-print('Python' in course)
+
+
+# x = 10 
+
+# x = x + 3 is the same as x += 3
