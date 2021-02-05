3458df1b4ce075c7a0e5c2ec21120c16d6ef46e9
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-02-04 20:26:25 -0800

    Conditionals + float conversion for input string

--- app.py
+++ app.py
@@ -42,6 +42,12 @@
 
 # print("Done")
 
-weight = input("Weight: ")
+weight = float(input("Weight: "))
 unit = input("(K)g or (L)bs: ")
 
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + converted)
+else: 
+    converted = weight * 0.45
+    print("Weight in Kgs: " + converted)
